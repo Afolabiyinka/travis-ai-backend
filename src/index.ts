ae149f38b9047ff9a2eccf692265fa89e2b37403
Ice Chain <afolabiyinka738@gmail.com>
2026-05-18 08:44:41 +0100

    fix: reorder CORS origins for consistency

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ app.use(cookieParser());
 //CORS setup
 app.use(
   cors({
-    origin: ["https://travis-ai.vercel.app", "http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://travis-ai.vercel.app",],
     credentials: true,
   })
 );
